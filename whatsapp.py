4974e3dad649ee665a40c8646e2a902798fa4596
David Pickel <pickeld@gmail.com>
2025-09-17 18:13:25 +0300

    Update media file saving path in MediaMessage class to use 'imgs/' directory for better organization

--- whatsapp.py
+++ whatsapp.py
@@ -24,7 +24,7 @@ class MediaMessage:
             if config.log_level == "DEBUG":
                 # save media to file
                 extension = self.type.split("/")[-1]
-                filename = f"media_{payload.get('id')}.{extension}"
+                filename = f"imgs/media_{payload.get('id')}.{extension}"
                 with open(filename, "wb") as f:
                     f.write(base64.b64decode(self.base64))
                 logger.debug(f"Saved media to {filename}")
