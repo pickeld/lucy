5cbe9660c2bac8ffca87afa3e373f9526e88f427
David Pickel <pickeld@gmail.com>
2025-09-17 16:12:23 +0300

    Add MediaMessage and QuotedMessage classes: Implement media handling and quoted message processing with base64 encoding and file management.

---
+++ whatsapp.py
@@ -0,0 +1,81 @@
+import base64
+
+from groups import Group, GroupManager
+import httpx
+import json
+from config import config
+from contact import ContactManager, Contact
+from utiles.globals import send_request
+from utiles.logger import logger
+
+contact_manager = ContactManager()
+group_manager = GroupManager()
+
+
+class MediaMessage:
+    def __init__(self, data):
+        # logger.debug(f"Message has media: {payload}")
+        self.url = data.get('url')
+        self.type = data.get('mimetype')
+        self.base64 = base64.standard_b64encode(httpx.get(
+            self.url, headers={"X-Api-Key": config.waha_api_key}).content).decode("utf-8")
+
+
+class QuotedMessage:
+    def __init__(self, quoted_data, recipient):
+        self.quoted_data = quoted_data
+        self.quoted_msg = quoted_data.get("quotedMsg", {})
+        self.type = self.quoted_msg.get("type", "")
+        self.body = self.quoted_msg.get("body", "").strip()
+        self.kind = self.quoted_msg.get("kind", "")
+        self.quoted_stanza_id = quoted_data.get("quotedStanzaID", "")
+        self.quoted_participant = quoted_data.get("quotedParticipant", "")
+        self.mimetype = self.quoted_msg.get("mimetype", "")
+        self.caption = self.quoted_msg.get("caption", "").strip()
+        if self.type == "image":
+            self.file_extension = self.mimetype.split("/")[-1]
+            self.filename = f"true_{recipient}_{self.quoted_stanza_id}_{self.quoted_participant}.{self.file_extension}"
+            endpoint = f"/api/files/default/{self.filename}"
+            response = send_request(method="GET", endpoint=endpoint)
+            self.base64_data = base64.b64encode(
+                response.content).decode("ascii")
+
+
+class WhatsappMSG:
+    def __init__(self, payload):
+        self.contact: Contact = contact_manager.get_contact(payload)
+        self.group: Group = group_manager.get_group(payload)
+
+        self.timestamp = payload.get("timestamp")
+        self.message = payload.get("body") if payload.get(
+            "body") else "Empty message"
+        # self.media = MediaMessage(payload)
+        # self.quoted = QuotedMessage(quoted_data=payload.get("quotedMsg", {}), recipient=self.recipient)
+        # self.recipient = payload.get("to")
+
+    def __str__(self) -> str:
+        return f"{self.timestamp} Message from {self.group.name}/{self.contact.name}: {self.message}"
+
+    def route(self):
+        if self.message.startswith(config.chat_prefix):
+            return "chat"
+        elif self.message.startswith(config.dalle_prefix):
+            return "dalle"
+        else:
+            return "unknown"
+
+    def reply(self, response: str):
+        send_request(method="POST",
+                     endpoint="/api/sendText",
+                     payload={
+                              "chatId": self.recipient,
+                              "text": response,
+                              "session": config.waha_session_name
+                     }
+                     )
+
+
+if __name__ == "__main__":
+    from samples import json_msg
+    msg = WhatsappMSG(json_msg)
+    print(msg)
